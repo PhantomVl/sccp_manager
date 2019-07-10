2bca6e4d8d9a24317833d9b11cc6da22080d5d19
PhantomVl <phv123@mail.ru>
2019-07-10 20:52:50 +0300

    - Add Video codecs supported in Global config

--- views/server.codec.php
+++ views/server.codec.php
@@ -10,6 +10,7 @@
 $def_val = null;
 $dev_id = null;
 $sccp_codec = $this->getCodecs('audio', true);
+$video_codecs = $this->getCodecs('video', true);
 $sccp_disalow_def = $this->extconfigs->getextConfig('sccpDefaults','disallow');
 $sccp_disalow = $sccp_disalow_def;
 
@@ -44,7 +45,7 @@ if (!empty($_REQUEST['id'])) {
     
     <!--SCCP Audio Codecs-->
     <div class="section-title" data-for="sccp_acodecs">
-        <h3><i class="fa fa-minus"></i><?php echo _("SCCP Codecs") ?></h3>
+        <h3><i class="fa fa-minus"></i><?php echo _("SCCP Audio Codecs ") ?></h3>
     </div>
     <div class="section" data-id="sccp_acodecs">
         <!--Codecs-->
@@ -54,7 +55,7 @@ if (!empty($_REQUEST['id'])) {
                     <div class="row">
                         <div class="form-group">
                             <div class="col-md-3">
-                                <label class="control-label" for="codecw"><?php echo _("SCCP Audio Codecs (allow)") ?></label>
+                                <label class="control-label" for="codecw"><?php echo _("Audio (allow)") ?></label>
                             </div>
                             <div class="col-md-9">
                                 <div>
@@ -90,6 +91,62 @@ if (!empty($_REQUEST['id'])) {
         </div>
         <!--END Codecs-->
 
+        <!--END SCCP Audio Codecs-->
+    </div>
+    <!--SCCP Video Codecs-->
+    <div class="section-title" data-for="sccp_vcodecs">
+        <h3><i class="fa fa-minus"></i><?php echo _("SCCP Video Codecs ") ?></h3>
+    </div>
+    <div class="section" data-id="sccp_vcodecs">
+        <!--Codecs-->
+        <div class="element-container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="form-group">
+                            <div class="col-md-3">
+                                <label class="control-label" for="codecw"><?php echo _("Video (allow)") ?></label>
+                            </div>
+                            <div class="col-md-9">
+                                <div>
+                                <?php echo show_help(_("This is the default Codec setting for SCCP Device.")) ?>
+                                </div>
+                                <?php
+                                $seq = 1;
+
+                                echo '<ul class="sortable">';
+                                foreach ($video_codecs as $codec => $codec_state) {
+                                    $codec_trans = _($codec);
+                                    $codec_checked = $codec_state ? 'checked' : '';
+                                    echo '<li><a href="#">'
+                                    . '<img src="assets/sipsettings/images/arrow_up_down.png" height="16" width="16" border="0" alt="move" style="float:none; margin-left:-6px; margin-bottom:-3px;cursor:move" /> '
+                                    . '<input type="checkbox" '
+                                    . ($codec_checked ? 'value="' . $seq++ . '" ' : '')
+                                    . 'name="voicecodecs[' . $codec . ']" '
+                                    . 'id="' . $codec . '" '
+                                    . 'class="audio-codecs" '
+                                    . $codec_checked
+                                    . ' />'
+                                    . '&nbsp;&nbsp;<label for="' . $codec . '"> '
+                                    . '<small>' . $codec_trans . '</small>'
+                                    . " </label></a></li>\n";
+                                }
+                                echo '</ul>';
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--END Codecs-->
+        <!--END SCCP Video Codecs-->
+    </div>
+
+    <div class="section-title" data-for="sccp_dcodecs">
+        <h3><i class="fa fa-minus"></i><?php echo _("SCCP Disallow Codecs ") ?></h3>
+    </div>
+    <div class="section" data-id="sccp_dcodecs">
 
         <!--Codec disallow-->
         <div class="element-container">
@@ -116,9 +173,9 @@ if (!empty($_REQUEST['id'])) {
             </div>
         </div>
         <!--END Codec disallow-->
-
-        <!--END SCCP Audio Codecs-->
     </div>
+
+    
 </form>
 
 
